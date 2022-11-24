991825023997b31b8b42ff7e9c0d83eb30e6d9b7
Christopher Oka <70914858+ChristopherOka@users.noreply.github.com>
2022-11-24 11:04:56 -0500

    Update 2_lighthouse.py

--- 2_lighthouse.py
+++ 2_lighthouse.py
@@ -4,40 +4,38 @@ import os
 import pandas as pd
 import random
 from datetime import datetime
-from subprocess import Popen
+import subprocess
+import re
 
-random_indices = random.sample(range(1, 5), 3)
+random_indices = random.sample(range(1, 854), 170)
 random_indices = sorted(random_indices)
 print(random_indices)
 
 df = pd.DataFrame([], columns=["SRS Index", 'URL','Accessibility Score'])
 
-df_urls = pd.read_csv("./inputs/urls_test.csv")["Website"]
+df_urls = pd.read_csv("./lower_market_caps.csv")["Website"]
 urls = df_urls.values.tolist()
 
 for index in random_indices:    
     bad_urls = ["nan", '']
-    if urls[index] in bad_urls or not urls[index]:
+    print(urls[index])
+    if str(urls[index]).strip() in bad_urls or not urls[index]:
+        print("bad url", urls[index])
         continue
     filename = str(index) + ".json"
-    stream = os.popen(f'lighthouse {urls[index]} --only-categories=accessibility --quiet --output=json --output-path=./output/{index}.json --chrome-flags="--headless"')
-    print("Report complete for: " + urls[index])
-
-    
-# process is stuck on creating new .json file from output... unknown reason why
-# try capturing terminal input directly intead of writing to a file first
-    while(not os.path.exists(f'./output/{index}.json')):
+    try:
+        stream = subprocess.run(f'lighthouse {str(urls[index]).strip()} --skip-audits=full-page-screenshot --only-categories=accessibility --output=json --chrome-flags="--headless', stdout=subprocess.PIPE, shell=True)
+    except:
+        print('ERROR: BAD URL', urls[index])
         continue
-
-    with open("./output/"+ str(index) + ".json") as json_data:
-        loaded_json = json.load(json_data)
-        score = str(round(loaded_json["categories"]["accessibility"]["score"] * 100))
-        print(score)
-        new_row = {
+    loaded_json = json.loads(str(stream.stdout.decode('utf-8')))
+    accessibility_score = score = str(round(loaded_json["categories"]["accessibility"]["score"] * 100))
+    print("Report complete for: " + str(urls[index]), "score:", accessibility_score)
+    new_row = {
             "SRS Index": random_indices[index],
             "URL": urls[index],
             "Accessibility Score": score
         }
-        df = pd.concat([df, pd.DataFrame([new_row])])
+    df = pd.concat([df, pd.DataFrame([new_row])])
 
 df.to_csv('./output/accessibility_scores.csv')
