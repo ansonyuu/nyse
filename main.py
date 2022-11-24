bb58a608f7718fba31db10e4c92f600e8fdc00c7
ansonyuu <ansonyu24@gmail.com>
2022-11-23 22:12:31 -0500

    feature: added website column

--- main.py
+++ main.py
@@ -10,23 +10,25 @@ for index, row in csv.iterrows():
     tickers += row["ACT Symbol"] + ' '
 
 # Gets all data from yfinance with specified tickers
-all_ticker_data = yf.Tickers(tickers)
-print(all_ticker_data)
+all_ticker_data = yf.Tickers(tickers).tickers
 
 # print(all_ticker_data.tickers['WWE'].info.get('marketCap'))
 
 # create empty dataframe
-df = pd.DataFrame(columns=["Ticker", "Company Name", "Market Cap"])
+df = pd.DataFrame(columns=["Ticker", "Company Name", "Market Cap", "Website"])
 
 for index, row in csv.iterrows():
     ticker = row["ACT Symbol"]
     name = row["Company Name"]
-    market_cap = all_ticker_data.tickers[ticker].info.get('marketCap')
+    yfinance_info = all_ticker_data[ticker].info
+    market_cap = yfinance_info.get('marketCap')
+    website = yfinance_info.get('website')
     print(ticker, name, market_cap)
     new_row = {
         "Ticker": ticker,
         "Company Name": name,
         "Market Cap": market_cap,
+        "Website": website
     }
     df = pd.concat([df, pd.DataFrame([new_row])])
 
